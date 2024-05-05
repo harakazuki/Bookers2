34d839502a9e86426f24477c459deaedd6f60fae
harakazuki <160248478+harakazuki@users.noreply.github.com>
2024-05-05 06:00:06 +0000

    books_right

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,14 +1,17 @@
 class BooksController < ApplicationController
   def create
-      @book.user_id = current_user.id
+      @book = current_user.books.build(book_params)
     if @book.save
       redirect_to books_path
     else
+      @books = Book.all
+      @user = current_user
       render :index
     end
   end
   
   def index
+    @books = Book.all
     @book = Book.new
     @user = current_user
   end
@@ -17,5 +20,9 @@ class BooksController < ApplicationController
   end
 
   def edit
+  end
+  
+  def book_params
+    params.require(:book).permit(:title, :body)
   end
 end
